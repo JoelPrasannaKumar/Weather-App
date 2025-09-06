c380de86f4d2915da0ed713cb2f398cac6cf56a7
JoelPrasannaKumar <joelprasannakumar2005@gmail.com>
2025-09-06 21:10:32 +0530

    Update tailwind.config.js

--- frontend1/tailwind.config.js
+++ frontend1/tailwind.config.js
@@ -3,10 +3,13 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}', // <-- Make sure this line is present
+    './app/**/*.{js,ts,jsx,tsx,mdx}', // <-- This line is the most important one for your structure
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
